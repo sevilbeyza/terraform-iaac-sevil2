db244ff0f717e909b2657785e12768fa30759c30
EC2 Default User <ec2-user@ip-172-31-5-215.us-west-1.compute.internal>
2020-05-13 23:23:30 +0000

    update

--- AWS/EKS/backend.tf
+++ AWS/EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform { 
 backend "s3" { 
 bucket = "sevil-terraform-eks" 
-key = "application/us-east-1/state/dev/infrastructer.tfstate.json" 
+key = "application/us-east-1/state/stage/infrastructure.tfstate_oregon.json" 
 region = "us-east-1" 
   } 
 } 
